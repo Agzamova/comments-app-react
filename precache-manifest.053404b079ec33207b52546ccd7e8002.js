7b0b915d94b6bbcbab144fbce28ef4545a4b622b
Agzamova <agzamova@inbox.ru>
2020-09-19 03:38:42 +0500

    Updates

---
+++ precache-manifest.053404b079ec33207b52546ccd7e8002.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fae65ed2ba948f077273abe18a21b8a7",
+    "url": "/comments-app-react/index.html"
+  },
+  {
+    "revision": "993ea34a60f289c60adc",
+    "url": "/comments-app-react/static/css/main.6350144e.chunk.css"
+  },
+  {
+    "revision": "029622a1cf286420ec0c",
+    "url": "/comments-app-react/static/js/2.be82a1bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/comments-app-react/static/js/2.be82a1bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "993ea34a60f289c60adc",
+    "url": "/comments-app-react/static/js/main.f0a62197.chunk.js"
+  },
+  {
+    "revision": "5ead7cf69cee172afdae",
+    "url": "/comments-app-react/static/js/runtime-main.9721a77c.js"
+  }
+]);
